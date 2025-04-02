cd4342127de19998382829732585e5ba79dd2b42
Anmol Sharma <anmolsharma2902@gmail.com>
2025-04-03 00:15:23 +0530

    Captain Authentication

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const captainController = require('../controllers/captain.controller');
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const { authCaptain } = require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -15,5 +16,15 @@ router.post('/register',[
     captainController.registerCaptain
 )
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be atleast 6 characters long'),
+],
+    captainController.loginCaptain
+)
+
+router.get('/profile', authCaptain , captainController.getCaptainProfile);
+
+router.get('/logout', authCaptain, captainController.logoutCaptain);
 
 module.exports = router;
